7899dbdbb9d1a4f7b72186d23ac90136b9c991fd
FlorisDeVries <florisdev@hotmail.com>
2019-04-10 16:55:35 +0200

    Random spawns Fixed zoom levels Labels now fade TimeScale fix...

--- GraviSim/Assets/ButtonFunctions.cs
+++ GraviSim/Assets/ButtonFunctions.cs
@@ -9,18 +9,6 @@ public class ButtonFunctions : MonoBehaviour
     private bool paused = true;
     public GravitySystem gs;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void PauseToggle(Button button){
         if(paused){
             button.GetComponentInChildren<TextMeshProUGUI>().text = "Pause";
--- GraviSim/Assets/GravityObject.cs
+++ GraviSim/Assets/GravityObject.cs
@@ -7,16 +7,57 @@ public class GravityObject : MonoBehaviour
 {
     // Mass of object (in kg/100000)
     public float mass = 1;
+
+    [Tooltip("X-axes describes the total velocity of the body(relative to the sun)")]
     public Vector3 velocity;
     public bool trackLabel = true;
+
+    [Tooltip("The body that the body will be revolving around")]
+    public GameObject baryCenter;
+    public float distanceToBaryCenter;
+
+    public List<GravityObject> satallites;
+
+    public float maxZoom = 5;
 
     // Should be refactored into the rocket class
     public TextMeshProUGUI text;
 
+
+    public Vector2 Rotate(Vector2 v, float degrees) {
+         float radians = degrees * Mathf.Deg2Rad;
+         float sin = Mathf.Sin(radians);
+         float cos = Mathf.Cos(radians);
+
+         return new Vector2(cos * v.x - sin * v.y, sin * v.x + cos * v.y);
+    }
+
+    public float RandomStart(GameObject center, float rotated = 0){
+            float randomAngleDegrees = rotated == 0 ? Random.Range(0, 360) : rotated;
+            float angleRadians = randomAngleDegrees * Mathf.PI / 180.0f;
+
+            float sin = Mathf.Sin(angleRadians);
+            float cos = Mathf.Cos(angleRadians);
+
+            float x = distanceToBaryCenter * sin;
+            float y = distanceToBaryCenter * cos;
+
+            Vector2 v2Velocity = Rotate(new Vector2(velocity.x, velocity.y), -randomAngleDegrees);
+            velocity = new Vector3(v2Velocity.x, 0, v2Velocity.y);            
+
+            transform.position = center.transform.position + new Vector3(x, 0, y);
+            return randomAngleDegrees;
+    }
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        if(baryCenter != null){
+            float randomAngleDegree = RandomStart(baryCenter);
+
+            foreach(GravityObject go in satallites){
+                go.RandomStart(this.gameObject, randomAngleDegree);
+            }
+        }
     }
 
     // Update is called once per frame
