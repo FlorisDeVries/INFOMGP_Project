817b2919c59c2006203b55075bcc469c50b16ca8
Niels Mulder <nc.mulder@outlook.com>
2019-04-10 19:49:51 +0200

    Added some included files

---
+++ GraviSim/Assets/LevelSaving.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static public class LevelSaving
+{
+    private static int levelsCleared = 1;
+    private static int latestLevelCleared = 0;
+
+    public static int LevelsCleared(){
+        return levelsCleared;
+    }
+
+    public static void LevelCleared(int i){
+        if(i <= latestLevelCleared) return;
+        latestLevelCleared++;
+        levelsCleared++;
+    }
+}
---
+++ GraviSim/Assets/SetLevelButtons.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetLevelButtons : MonoBehaviour
+{
+    public Button[] levelButtons;
+    public GameObject[] buttonHolders;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int levelsCleared = LevelSaving.LevelsCleared();
+        for(int i = 0; i < levelsCleared; i++){
+            levelButtons[i].interactable = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
