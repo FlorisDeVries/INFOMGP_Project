74332218e24b03b06bb90c29a4de721fdd197510
Niels Mulder <nc.mulder@outlook.com>
2019-04-10 21:59:15 +0200

    Level 3 Fly-bies

---
+++ GraviSim/Assets/ReportFlyBy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReportFlyBy : MonoBehaviour
+{
+    public GoalCollision gC;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider){
+        print($"Flew by {this.transform.parent.name}");
+        if(collider.name == "Rocket"){
+            gC.alertFlyBy(this.gameObject);
+        }
+    }
+}
